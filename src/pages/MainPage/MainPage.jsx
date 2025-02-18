ec382f32d10a7ca5117fd469f813d34fa820efc9
Amy <xyl.amy@gmail.com>
2025-02-17 17:06:39 -0800

    Create main page layout with hero photo & title. Adjust some font sizing in globals

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,19 @@
 import "./MainPage.scss";
 
 function MainPage() {
-  return <div></div>;
+  return (
+    <section className="main-page">
+      <article className="main-page__hero">
+        <img
+          src="/src/assets/images/fridge.jpg"
+          className="main-page__hero-img"
+        />
+      </article>
+      <article className="main-page__content">
+        <h1 className="main-page__content-title">Select your ingredients:</h1>
+      </article>
+    </section>
+  );
 }
 
 export default MainPage;
