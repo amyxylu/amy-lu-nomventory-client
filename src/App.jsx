f6a559d7a14efc1234466bceff77063e67766caf
Amy <xyl.amy@gmail.com>
2025-02-18 22:27:06 -0800

    create CusinesPage and CuisineRecipesPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import MainPage from "./pages/MainPage/MainPage";
 import RecipesPage from "./pages/RecipesPage/RecipesPage";
+import CuisinesPage from "./pages/CuisinesPage/CuisinesPage";
+import CuisineRecipesPage from "./pages/CuisineRecipesPage/CuisineRecipesPage";
 import RecipeDetailsPage from "./pages/RecipeDetailsPage/RecipeDetailsPage";
 import PreferencesPage from "./pages/PreferencesPage/PreferencesPage";
 import Footer from "./components/Footer/Footer";
@@ -15,8 +17,8 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/recipes" element={<RecipesPage />} />
         <Route path="/recipes/:recipeId" element={<RecipeDetailsPage />} />
-        <Route path="/cuisines" element="" />
-        <Route path="/cuisines/:cuisineType" element="" />
+        <Route path="/cuisines" element={<CuisinesPage />} />
+        <Route path="/cuisines/:cuisineType" element={<CuisineRecipesPage />} />
         <Route path="/preferences" element={<PreferencesPage />} />
       </Routes>
       <Footer />
---
+++ src/pages/CuisinesPage/CuisinesPage.jsx
@@ -0,0 +1,11 @@
+import "./CuisinesPage.scss";
+
+function CuisinesPage() {
+  return (
+    <section className="cuisines-page">
+      <h1 className="cuisines-page__header">Choose Your Cuisine!</h1>
+    </section>
+  );
+}
+
+export default CuisinesPage;
