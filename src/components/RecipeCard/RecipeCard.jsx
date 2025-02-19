c6895ca0de8bf8b858ed661ad09a9b4f8a4f41ae
Amy <xyl.amy@gmail.com>
2025-02-18 16:14:43 -0800

    Add useEffect function to convert cuisine_id to cuisineName, create list item buttons to display key data

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./RecipeCard.scss";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
 function RecipeCard({ recipe }) {
+  const [cuisineName, setCuisineName] = useState("");
+
+  useEffect(() => {
+    const getCuisineName = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/api/cuisines`);
+        const cuisines = response.data;
+
+        const matchedCuisine = cuisines.find(
+          (cuisine) => cuisine.id === recipe.cuisine_id
+        );
+        setCuisineName(
+          matchedCuisine ? matchedCuisine.cuisine_name : "Unknown Cuisine"
+        );
+      } catch (error) {
+        console.error("Error fetching cuisines:", error);
+        setCuisineName("?");
+      }
+    };
+    getCuisineName();
+  }, [recipe.cuisine_id]);
+
   const imageUrl = recipe.image_url
     ? `${BASE_URL}${
         recipe.image_url.startsWith("/")
@@ -10,6 +34,8 @@ function RecipeCard({ recipe }) {
           : `/${recipe.image_url}`
       }`
     : "/src/assets/images/fridge.jpg";
+
+  const totalCookTime = (recipe.prep_time || 0) + (recipe.cook_time || 0);
 
   return (
     <article className="recipe-card">
@@ -20,6 +46,14 @@ function RecipeCard({ recipe }) {
           className="recipe-card__image"
         />
       )}
+      <div className="recipe-card__content">
+        <h3 className="recipe-card__header">{recipe.recipe_name}</h3>
+        <ul className="recipe-card__buttons">
+          <li className="recipe-card__btn">{totalCookTime} min</li>
+          <li className="recipe-card__btn">{recipe.difficulty_level}</li>
+          <li className="recipe-card__btn">{cuisineName}</li>
+        </ul>
+      </div>
     </article>
   );
 }
