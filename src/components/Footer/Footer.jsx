c12dc01a6a4bce973e545942430821b43fa507d8
Amy <xyl.amy@gmail.com>
2025-02-20 19:14:43 -0800

    add background music

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,11 @@ import "./Footer.scss";
 function Footer() {
   return (
     <footer className="footer">
+      <audio
+        controls
+        className="footer__audio"
+        src="/src/assets/media/Tsundere_Head_Empty.mp3"
+      />
       <p className="footer__text">
         &copy; 2025 Amyxylu Innovations | All Rights Reserved
       </p>
